2dbaef20b7f9c6becfb8f0e84437487e028e0c64
na2kera <nat3mitaro@gmail.com>
2025-01-06 17:20:22 +0900

    アイコンを表示

--- app/components/MapPage.tsx
+++ app/components/MapPage.tsx
@@ -3,18 +3,34 @@
 import React, { useState } from "react";
 import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+const customIcon = L.icon({
+  iconUrl: "/images/marker-icon.png",
+  shadowUrl: "/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
+L.Marker.prototype.options.icon = customIcon;
 
 const MapPage = () => {
-  const [position, setPosition] = useState(null);
+  const [position, setPosition] = useState<{ lat: number; lng: number } | null>(
+    null
+  );
 
   function LocationMarker() {
     useMapEvents({
-      click(e) {
+      click(e: { latlng: { lat: number; lng: number } }) {
         setPosition(e.latlng);
       },
     });
 
-    return position === null ? null : <Marker position={position}></Marker>;
+    return position === null ? null : (
+      <Marker position={position} icon={customIcon}></Marker>
+    );
   }
 
   return (
