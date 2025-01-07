a98bb45a9ee31777ef9cd8ab2858454d430c072a
na2kera <nat3mitaro@gmail.com>
2025-01-07 15:14:18 +0900

    UI修正

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -6,37 +6,26 @@ type FormProps = {
 };
 
 const Form = ({ lat, lon }: FormProps) => {
-  const [area, setArea] = useState("tokyo");
   const [houseArea, setHouseArea] = useState(0);
   const [afterYear, setAfterYear] = useState(0);
-  const [PTN2020, setPTN2020] = useState(0);
-  const [landPrice, setLandPrice] = useState(0);
   const [madoriNumberAll, setMadoriNumberAll] = useState(0);
   const [floorCount, setFloorCount] = useState(0);
-  const [roomCount, setRoomCount] = useState(0);
+
+  const [predictPrice, setPredictPrice] = useState("");
 
   const handleSubmit = async () => {
-    console.log(
-      area,
-      houseArea,
-      afterYear,
-      PTN2020,
-      landPrice,
-      madoriNumberAll,
-      floorCount,
-      roomCount
-    );
+    console.log(houseArea, afterYear, madoriNumberAll, floorCount);
     const sendData = {
-      area: area,
+      area: "tokyo",
       house_area: houseArea,
       after_year: afterYear,
       lat: lat,
       lon: lon,
-      PTN_2020: PTN2020,
-      landprice: landPrice,
+      PTN_2020: 4069.4293,
+      landprice: 719000,
       madori_number_all: madoriNumberAll,
       floor_count: floorCount,
-      room_count: roomCount,
+      room_count: madoriNumberAll,
     };
     const response = await fetch("http://127.0.0.1:8000/predict", {
       method: "POST",
@@ -46,52 +35,55 @@ const Form = ({ lat, lon }: FormProps) => {
       body: JSON.stringify(sendData),
     });
     const result = await response.json();
-    console.log(result);
+    console.log(result.prediction[0][0]);
+    setPredictPrice(result.prediction[0][0]);
   };
 
   return (
     <>
-      <input
-        type="text"
-        value={area}
-        onChange={(e) => setArea(e.target.value)}
-      />
-      <input
-        type="number"
-        value={houseArea}
-        onChange={(e) => setHouseArea(Number(e.target.value))}
-      />
-      <input
-        type="number"
-        value={afterYear}
-        onChange={(e) => setAfterYear(Number(e.target.value))}
-      />
-      <input
-        type="number"
-        value={PTN2020}
-        onChange={(e) => setPTN2020(Number(e.target.value))}
-      />
-      <input
-        type="number"
-        value={landPrice}
-        onChange={(e) => setLandPrice(Number(e.target.value))}
-      />
-      <input
-        type="number"
-        value={madoriNumberAll}
-        onChange={(e) => setMadoriNumberAll(Number(e.target.value))}
-      />
-      <input
-        type="number"
-        value={floorCount}
-        onChange={(e) => setFloorCount(Number(e.target.value))}
-      />
-      <input
-        type="number"
-        value={roomCount}
-        onChange={(e) => setRoomCount(Number(e.target.value))}
-      />
-      <button onClick={handleSubmit}>予測</button>
+      <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2">
+          <label>建物面積（平米）</label>
+          <input
+            type="number"
+            value={houseArea}
+            onChange={(e) => setHouseArea(Number(e.target.value))}
+            className="border p-2 rounded mb-2 w-full"
+          />
+          <label>築年数（年）</label>
+          <input
+            type="number"
+            value={afterYear}
+            onChange={(e) => setAfterYear(Number(e.target.value))}
+            className="border p-2 rounded mb-2 w-full"
+          />
+          <label>部屋の数（室）</label>
+          <input
+            type="number"
+            value={madoriNumberAll}
+            onChange={(e) => setMadoriNumberAll(Number(e.target.value))}
+            className="border p-2 rounded mb-2 w-full"
+          />
+          <label>階数（階）</label>
+          <input
+            type="number"
+            value={floorCount}
+            onChange={(e) => setFloorCount(Number(e.target.value))}
+            className="border p-2 rounded mb-2 w-full"
+          />
+          <button
+            onClick={handleSubmit}
+            className="bg-blue-500 text-white p-2 rounded w-full hover:bg-blue-600"
+          >
+            予測
+          </button>
+        </div>
+        <div className="flex flex-col gap-2">
+          <p className="text-2xl font-bold">
+            予想価格：{predictPrice.toLocaleString()}万円
+          </p>
+        </div>
+      </div>
     </>
   );
 };
--- app/components/MapPage.tsx
+++ app/components/MapPage.tsx
@@ -18,9 +18,10 @@ const customIcon = L.icon({
 L.Marker.prototype.options.icon = customIcon;
 
 const MapPage = () => {
-  const [position, setPosition] = useState<{ lat: number; lng: number } | null>(
-    null
-  );
+  const [position, setPosition] = useState<{ lat: number; lng: number }>({
+    lat: 35.6895,
+    lng: 139.6917,
+  });
 
   function LocationMarker() {
     useMapEvents({
@@ -36,24 +37,26 @@ const MapPage = () => {
 
   return (
     <>
-      <MapContainer
-        center={[35.6895, 139.6917]}
-        zoom={13}
-        style={{ height: "400px", width: "100%" }}
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <LocationMarker />
-      </MapContainer>
-      {position && (
-        <p>
-          選択した座標: 緯度 {position.lat.toFixed(4)}, 経度{" "}
-          {position.lng.toFixed(4)}
-        </p>
-      )}
-      <Form lat={position?.lat ?? 0} lon={position?.lng ?? 0} />
+      <div className="flex flex-col gap-2 max-w-3xl mx-auto w-11/12">
+        <MapContainer
+          center={[35.6895, 139.6917]}
+          zoom={13}
+          style={{ height: "400px", width: "100%" }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <LocationMarker />
+        </MapContainer>
+        {position && (
+          <p>
+            選択した座標: 緯度 {position.lat.toFixed(4)}, 経度{" "}
+            {position.lng.toFixed(4)}
+          </p>
+        )}
+        <Form lat={position.lat} lon={position.lng} />
+      </div>
     </>
   );
 };
